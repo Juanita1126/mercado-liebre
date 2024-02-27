cd3817ca38f468cfb42d7e71e7d0210c1c5093b3
juanita <lvggs11@gmail.com>
2024-02-27 18:15:48 -0500

    finish

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// esta funcion marca que en "/mercadoLiebre" vas a poder acceder al html que vos estas usando
+app.get('', (req, res) =>{  
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+}); 
+app.get('/login', (req, res) =>{  
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+app.get('/register', (req, res) =>{  
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+app.use(express.static(path.join(__dirname, './public')));  
+
+//el primer parametro del listen dice en que numero de "localhost:" va a estar
+app.listen(process.env.PORT || 3002, () => {
+    console.log("Servidor corriendo");
+});
+
+//Escribir "npm start" para levantar el servidor
+
